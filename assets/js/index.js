da362ab2a0e2075fcf683f38a00ac8332ffcc752
VitorSMaia <vitor.smaia1@gmail.com>
2023-05-10 12:02:19 -0300

    Ajuste de Tela

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,7 +16,6 @@ function loadPokemonItems(offset,limit) {
     PokeApi.getPokemons(offset,limit).then((PokeApiList = []) => {
 
         pokemonList.innerHTML = PokeApiList.map((pokemon) => convertToHTML(pokemon)).join('');
-        
     })
     
 }
@@ -30,8 +29,8 @@ function convertToHTML(pokemon) {
       <p class="text-xs">#${pokemon.number}</p>
       </div>
       <div class="grid grid-cols-2">
-        <div class="flex flex-wrap  gap-2 justify-start items-center gap-x-2">
-        ${ pokemon.types.map((type) => `<p class="px-3 text-sm rounded-full font-thin shadow-xl border-2 border-gray-300 ${type}">${type}</p>`).join('')}
+        <div class="flex flex-wrap justify-start gap-2">
+        ${ pokemon.types.map((type) => `<p class="px-3 text-sm w-max h-max rounded-full font-thin shadow-xl border-2 border-gray-300 ${type}">${type}</p>`).join('')}
         </div>
         <div class="flex justify-end items-center">
           <img class="h-40 w-40 drop-shadow-2xl" src="${pokemon.img}"
